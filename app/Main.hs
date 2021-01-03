6e1134e3924e589e7df4476e70401a696d83c387
adamliu84 <adam.liu84@gmail.com>
2021-01-03 11:08:11 +0800

    Simple strategy input selection

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,35 @@ module Main where
 
 import qualified Momentum (query)
 import qualified SPFive (query)
+import System.Exit (die)
+
+type StrategyList = (String, (String, IO ()))
 
 main :: IO ()
--- main = SPFive.query
-main = Momentum.query
+main = do
+  let loop = do
+        let strategyList = listStrategy
+        printStrategy strategyList
+        cmd <- getLine
+        case cmd of
+          "q" -> die "Exiting....."
+          _ -> case lookup cmd strategyList of
+            Just v -> snd v
+            Nothing -> putStrLn "Not within the function call"
+        loop
+  loop
+  where
+    listStrategy :: [StrategyList]
+    listStrategy =
+      zip
+        (map show [1 ..])
+        [ ("Equal-Weight S&P 500", SPFive.query),
+          ("Momentum Investing Strategy", Momentum.query)
+        ]
+    printStrategy :: [StrategyList] -> IO ()
+    printStrategy strategies =
+      do
+        putStrLn "List of strategies:"
+        mapM_ (\(x, (y, _)) -> putStrLn $ x ++ ": " ++ y) strategies
+        putStrLn "q: To quit gracefully"
+        putStrLn "-----------"
